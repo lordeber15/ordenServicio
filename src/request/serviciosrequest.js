28f9da7ba23b9693f56aaa42a54ba70cc781f173
lordeber15 <eberpalomino@hotmail.com>
2025-08-07 00:37:03 -0500

    ssl

--- src/request/serviciosrequest.js
+++ src/request/serviciosrequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const serviciosApi = axios.create({
-  baseURL: "http://impalexander.store:3000",
+  baseURL: "https://impalexander.store/api/",
   //baseURL: "https://imprenta-imprenta-a2bsyh-c1440d-62-171-181-168.traefik.me/",
 });
 export const getServicios = async () => {
