2799d2e8f316bd8ed662db9bebda55472c7f9ac4
Eberson Palomino <eberpalomino@hotmail.com>
2025-10-07 10:10:25 -0500

    Update productos.js

--- src/request/productos.js
+++ src/request/productos.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const productoApi = axios.create({
-  baseURL: "https://impalexander.store/api/",
+  baseURL: "https://api.impalexander.store/api/",
 });
 export const getProducto = async () => {
   const res = await productoApi.get("/producto");
